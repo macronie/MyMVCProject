d0a8ed618356f224a5db98dad832deecf2742949
macronie <poornima08@yahoo.co.in>
2018-05-15 22:18:09 -0500

    Commit upto Display BookList.

---
+++ MyMVCProject/Controllers/ReaderController.cs
@@ -0,0 +1,66 @@
+﻿using MyMVCProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MyMVCProject.Controllers
+{
+    public class ReaderController : Controller
+    {
+
+        // GET: Reader
+        public ActionResult Reader()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Reader(Reader reader)
+        {
+
+            MyDataBaseEntities dc = new MyDataBaseEntities();
+            var model = dc.ReadingLog.ToList();
+            try
+            {
+                dc.Reader.Add(reader);
+                dc.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                Console.WriteLine(ex.InnerException.Message);
+            }
+
+            return View();
+        }
+
+        public ActionResult ReaderList()
+        {
+
+            MyDataBaseEntities dc = new MyDataBaseEntities();
+            var readerList = dc.Reader.ToList();
+            return View(readerList);
+        }
+
+        [HttpGet]
+        public ActionResult ReaderDetails(int id)
+        {
+
+            MyDataBaseEntities dc = new MyDataBaseEntities();
+            var readingLogList = dc.ReadingLog.Where(a => a.ReaderId == id).ToList();
+            if (readingLogList != null)
+            {
+                Session["ReaderName"] = readingLogList[0].ReaderName;
+                Session["ReaderId"] = readingLogList[0].ReaderId;
+            }
+            return View(readingLogList);
+        }
+
+        
+        
+    }
+}
---
+++ MyMVCProject/Controllers/ReadingLogsController.cs
@@ -0,0 +1,64 @@
+﻿using MyMVCProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCProject.Controllers
+{
+    public class ReadingLogsController : Controller
+    {
+        // GET: ReadingLogs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult BookList()
+        {
+            using (MyDataBaseEntities dc = new MyDataBaseEntities())
+            {
+                int Id = Convert.ToInt32(Session["ReaderId"]);
+                var readingLogList = dc.ReadingLog.Where(a => a.ReaderId == Id).ToList();
+                //var BookList = (readingLogList.Select(a => a.BookName)).ToList();
+                //dc.ReadingLog.Where(a => a.ReaderId == readingLog.ReaderId).Select(a =>a.BookName).ToList();
+                //return Redirect(ReaderDetails);
+                //Console.Write(BookList);
+                return View("BookList", readingLogList);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Create(ReadingLog readingLog)
+        {
+            string bookName = readingLog.BookName;
+            string authorName = readingLog.Author;
+            readingLog.ClockIn = DateTime.Now;
+            readingLog.ClockOut = DateTime.Now;
+            readingLog.ReaderId = Convert.ToInt32(Session["ReaderId"]);
+            readingLog.ReaderName = Convert.ToString(Session["ReaderName"]);
+            int Id = Convert.ToInt32(Session["ReaderId"]);
+            using (MyDataBaseEntities dc = new MyDataBaseEntities())
+            {
+                dc.ReadingLog.Add(readingLog);
+                dc.SaveChanges();
+                var readingLogList = dc.ReadingLog.Where(a => a.ReaderId == Id).ToList();
+                //var BookList = (readingLogList.Select(a => a.BookName)).ToList();
+                //dc.ReadingLog.Where(a => a.ReaderId == readingLog.ReaderId).Select(a =>a.BookName).ToList();
+                //return Redirect(ReaderDetails);
+                //Console.Write(BookList);
+                return View("BookList", readingLogList);
+            }
+            
+        }
+    }
+}
